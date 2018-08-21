62fb16b2b620699e7ce1b5851c1d6b451341a598
jakob <jakobreichl@gmail.com>
2018-08-21 10:53:09 +0200

    Praktikum1: Lösung gleitkommazahlenLsg hinzugefügt
    Changes to be committed:
    	new file:   prakt/gleitkommazahlenLsg/build/.qmake.stash
    	new file:   prakt/gleitkommazahlenLsg/build/debug/gleitkommazahlenLsg.exe
    	new file:   prakt/gleitkommazahlenLsg/gleitkommazahlenLsg.pro
    	new file:   prakt/gleitkommazahlenLsg/main.c

---
+++ prakt/gleitkommazahlenLsg/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+  float z1, z2, sum;
+
+  printf(" Erste Zahl eingeben:\n");
+  scanf("%f", &z1 );
+  printf(" Zweite Zahl eingeben:\n");
+  scanf("%f", &z2 );
+  sum = z1 + z2;
+  printf("Zahl1: %f Zahl2: %f\n",z1, z2);
+
+  if ( sum < 0.0 ) {
+       printf("Ergebnis ist negativ!");
+  } else {
+       printf("Summe : %f\n",sum);
+       printf("Ergebnis ist positiv!");
+  }
+  return 0;
+}
