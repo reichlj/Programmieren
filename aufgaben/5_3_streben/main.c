1e3093a23457607889ced60cdd15ec9acefa15fc
jakob <jakobreichl@gmail.com>
2018-12-03 11:27:58 +0100

    Algorithmus vereinfacht und verbessert

--- aufgaben/5_3_streben/main.c
+++ aufgaben/5_3_streben/main.c
@@ -4,8 +4,8 @@ double stabneu(int n, double laenge, double winkel);
 
 int main(void)
 {
-  int anzahl;
-  double schenkel,strebe,gesamtlaenge,alpha;
+  int anzahl,nummer;
+  double schenkel,strebe,laenge,gesamtlaenge,alpha;
 
   printf("Laenge des Stabes eingeben : ");
   scanf("%lf",&schenkel);
@@ -15,28 +15,55 @@ int main(void)
   printf("Winkel in Grad =%7.3f\n",alpha);
 
   alpha=M_PI*alpha/180.0;
-
-  if (alpha >= M_PI/2) {
+  if (alpha >= M_PI/2)
+  {
      printf("Winkel groesser gleich 90 - keine Streben\n");
      return 0;
   }
-  anzahl=1;
-  strebe= stabneu(anzahl,schenkel,alpha);
-  if ( strebe < 0.01*schenkel ) {
-     printf("Erste Strebe bereits kleiner als 1/100\n");
-     return 0;
-  }
+
+  nummer = 1;
+  anzahl = 0;
   gesamtlaenge=0.0;
-  printf("Nummer  Strebe  Gesamtlaenge\n");
-  while ( (strebe>0.01*schenkel) && (anzahl <=20) )
+  laenge = schenkel;
+  while (1 == 1)
   {
-     gesamtlaenge = gesamtlaenge + strebe;
-     printf("%5d%9.4f%14.4f\n",anzahl,strebe,gesamtlaenge);
-     anzahl++;
-     strebe=stabneu(anzahl,strebe,alpha);
+      strebe= stabneu(nummer,laenge,alpha);
+      if (nummer == 1)
+      {
+          if (strebe < 0.01*schenkel)
+          {
+               printf("Erste Strebe bereits kleiner als 1/100\n");
+               break;                 // Schleife verlassen
+          }
+          else
+          {
+               printf("Nummer  Strebe  Gesamtlaenge\n");
+          }
+      }
+      if (anzahl < 20)
+      {
+          if (strebe >= 0.01*schenkel)
+          {
+               anzahl = anzahl + 1;       // strebe ist zulässig
+               gesamtlaenge = gesamtlaenge + strebe;
+               printf("%5d%9.4f%14.4f\n",nummer,strebe,gesamtlaenge);
+          }
+          else
+          {
+               break;                   // strebe zu kurz; Schleife verlassen
+          }
+      }
+      else
+      {                     // bereits 20 Streben ausgegeben
+           if (strebe >= 0.01*schenkel)
+               printf("Mehr als 20 Streben erforderlich\n");
+           else
+               printf("Genau 20 Streben erforderlich\n");
+           break;           // Schleife verlassen
+      }
+      nummer++;
+      laenge = strebe;
   }
-  if ( (strebe>0.01*schenkel) && (anzahl == 20) )
-     printf("Mehr als 20 Streben erforderlich\n");
   return 0;
 }
 
