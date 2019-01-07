a2dcbd6d8b2ad510ea6420e0a8617f5f1b36833a
jakob <jakobreichl@gmail.com>
2019-01-07 15:39:02 +0100

    Kap 7 Aufgabe 3 Lösung verbessert

--- aufgaben/7_3_zeit_millis/main.c
+++ aufgaben/7_3_zeit_millis/main.c
@@ -4,10 +4,10 @@ int main()
 {
     int m,s,ms;
     double t;
-    t = 312.1234;
+    t = 312.1064;
     berechne(t,&m,&s,&ms);
     printf("t=%7.5f : m=%d s=%d ms=%d\n",t,m,s,ms);
-    t = 312.1238;
+    t = 312.1068;
     berechne(t,&m,&s,&ms);
     printf("t=%7.5f : m=%d s=%d ms=%d\n",t,m,s,ms);
     return 0;
@@ -15,11 +15,11 @@ int main()
 void berechne(double t, int* z_m, int* z_s, int* z_ms)
 {
     int sek;
-    double rest;
-    sek = (int) t;
+    double restInMillis;
+    sek = (int) t;  // Castoperator
     *z_m = sek/60;
     *z_s = sek%60;
-    //rest = t - (int)t;
-    rest = 0.0005 + t - (int)t;
-    *z_ms = (int) ( rest*1000);
+    restInMillis = 1000 * ( t - (int)t );
+    *z_ms = (int) restInMillis; // abschneiden
+    *z_ms = (int) (0.5 + restInMillis); // runden
 }
